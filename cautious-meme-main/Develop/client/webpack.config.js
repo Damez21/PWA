fee5638896b3b20453264a04bc35294ea67d4cb5
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-08-07 18:24:30 -0400

    added rules and worked on css.

--- cautious-meme-main/Develop/client/webpack.config.js
+++ cautious-meme-main/Develop/client/webpack.config.js
@@ -23,7 +23,8 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        { test: /\.css$/, use: 'css-loader' },
+        { test: /\.ts$/, use: 'ts-loader' },
       ],
     },
   };
